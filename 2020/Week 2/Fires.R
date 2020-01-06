ca1bb3ca0c63d0a58fe4c4b3c553727f43ce4bdb
npaterno <nicholas.paterno@gmail.com>
2020-01-06 10:51:38 -0800

    Initial Commit Just getting the data and checking out the map between classes.

---
+++ 2020/Week 2/Fires.R
@@ -0,0 +1,37 @@
+library(tidyverse)
+
+rainfall <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-01-07/rainfall.csv')
+temperature <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-01-07/temperature.csv')
+
+# Mapping NSW Current Incidents in R -------------------------------------------
+# AUTHOR: DEAN MARCHIORI
+library(sf)
+library(mapview)
+library(tidyverse)
+
+#' Current Incidents Feed (GeoJSON)
+#' This feed contains a list of current incidents from the NSW RFS, 
+#' and includes location data and Major Fire Update summary information where available. 
+#' Click through from the feed to the NSW RFS website for full details of the update. 
+#' GeoJSON is a lightweight data standard that has emerged to support the sharing of 
+#' information with location or geospatial data. 
+#' It is widely supported by modern applications and mobile devices.
+
+url <- "http://www.rfs.nsw.gov.au/feeds/majorIncidents.json"
+
+fires <- st_read(url)
+
+fires
+
+mapview(fires)
+
+#' Hacky way to get rid of points within geometry collections
+fire_poly <- fires %>% 
+  st_buffer(dist = 0) %>% 
+  st_union(by_feature = TRUE)
+
+mapview(fire_poly)
+
+fires %>% 
+  mutate(pubdate = as.character(pubDate),
+         pubdate = as.Date(pubdate))
