7a78765321c00ba1770993dd2a60cd65a3a4c56b
Nick <Nick@Nicks-Air.hsd1.wa.comcast.net>
2020-01-06 21:29:25 -0800

    Added some data wrangling and started plots

--- 2020/Week 2/Fires.R
+++ 2020/Week 2/Fires.R
@@ -1,7 +1,30 @@
 library(tidyverse)
+library(lubridate)
 
 rainfall <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-01-07/rainfall.csv')
 temperature <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-01-07/temperature.csv')
+
+temp_city_year <- temperature %>% 
+  mutate(yr = year(date),
+         city_name = str_to_title(city_name)) %>% 
+  select(-c(date, site_name)) %>% 
+  filter(temp_type == "max") %>% 
+  group_by(city_name, yr) %>% 
+  summarize(mean_temp = mean(temperature, na.rm = TRUE))
+
+rainfall_city_yr <- rainfall %>% 
+  rename(yr = year,
+         rain = rainfall)%>% 
+  mutate(rain = replace_na(rain,0)) %>% 
+  select(city_name, yr, rain) %>% 
+  group_by(city_name, yr) %>% 
+  summarize(mean_rain = mean(rain, na.rm = TRUE))
+
+plot_data <- left_join(rainfall_city_yr,temp_city_year, by = c("city_name", "yr"))
+
+ggplot(plot_data, aes(yr, mean_temp, group = city_name, color = city_name))+
+  geom_smooth(na.rm = TRUE)
+
 
 # Mapping NSW Current Incidents in R -------------------------------------------
 # AUTHOR: DEAN MARCHIORI
