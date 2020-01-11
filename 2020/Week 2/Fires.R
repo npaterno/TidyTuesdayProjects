d7ef9d574559b47bb7c7e26a91ad26189c6cccd5
Nick <Nick@Nicks-Air.hsd1.wa.comcast.net>
2020-01-10 21:35:51 -0800

    *some plot changes. not sure I'll have time to finish this week since I have exams to grade.

--- 2020/Week 2/Fires.R
+++ 2020/Week 2/Fires.R
@@ -4,15 +4,11 @@ library(lubridate)
 rainfall <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-01-07/rainfall.csv')
 temperature <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-01-07/temperature.csv')
 
-ggplot(data = temperature, 
-       mapping = aes(x = date, y = temperature, color = city_name))+
-  geom_point(na.rm = TRUE)
-
-xtemp_city_year <- temperature %>% 
+temp_city_year <- temperature %>% 
   mutate(yr = year(date),
          city_name = str_to_title(city_name)) %>% 
   select(-c(date, site_name)) %>% 
-  filter(temp_type == "max") %>% 
+  #filter(temp_type == "max") %>% 
   group_by(city_name, yr) %>% 
   summarize(mean_temp = mean(temperature, na.rm = TRUE))
 
@@ -26,39 +22,5 @@ rainfall_city_yr <- rainfall %>%
 
 plot_data <- left_join(rainfall_city_yr,temp_city_year, by = c("city_name", "yr"))
 
-ggplot(plot_data, aes(yr, mean_temp, group = city_name, color = city_name))+
-  geom_smooth(na.rm = TRUE)
-
-
-# Mapping NSW Current Incidents in R -------------------------------------------
-# AUTHOR: DEAN MARCHIORI
-library(sf)
-library(mapview)
-library(tidyverse)
-
-#' Current Incidents Feed (GeoJSON)
-#' This feed contains a list of current incidents from the NSW RFS, 
-#' and includes location data and Major Fire Update summary information where available. 
-#' Click through from the feed to the NSW RFS website for full details of the update. 
-#' GeoJSON is a lightweight data standard that has emerged to support the sharing of 
-#' information with location or geospatial data. 
-#' It is widely supported by modern applications and mobile devices.
-
-url <- "http://www.rfs.nsw.gov.au/feeds/majorIncidents.json"
-
-fires <- st_read(url)
-
-fires
-
-mapview(fires)
-
-#' Hacky way to get rid of points within geometry collections
-fire_poly <- fires %>% 
-  st_buffer(dist = 0) %>% 
-  st_union(by_feature = TRUE)
-
-mapview(fire_poly)
-
-fires %>% 
-  mutate(pubdate = as.character(pubDate),
-         pubdate = as.Date(pubdate))
+ggplot(plot_data %>% filter(yr>1999), aes(yr, mean_temp, group = city_name, color = city_name))+
+  geom_smooth(na.rm = TRUE, se = FALSE)
