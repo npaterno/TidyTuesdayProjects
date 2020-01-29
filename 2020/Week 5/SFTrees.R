be220ded0f67dc79852d7052a08cd77b3a46b2dc
Nick Paterno <npaterno@Nicks-MacBook-Pro.local>
2020-01-29 11:47:15 -0800

    More work on animation. Still outputting png files and not a gif

--- 2020/Week 5/SFTrees.R
+++ 2020/Week 5/SFTrees.R
@@ -1,6 +1,9 @@
 library(tidyverse)
 library(ggmap)
 library(gganimate)
+library(gifski)
+
+altrenderer <- gifski_renderer(loop = FALSE)
 
 sf_trees <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-01-28/sf_trees.csv')
 
@@ -11,31 +14,41 @@ sf_map <- get_map(location = c(left = -122.53,
                   zoom = 16,
                   color = "bw")
 
-#sf_trees_clean <- sf_trees %>% 
-#  separate(site_info, 
-#           into = c("site_main","site_sub_1","site_sub_2"), 
-#           sep = ":") %>% 
-#  separate(species, 
-#           into = c("species","sub_species"),
-#           sep = "::") %>% 
-#  select(-c("address","tree_id","dbh","plot_size"))
+sf_plot_data <- sf_trees %>% 
+  filter(!is.na(date)) %>% 
+  separate(date, 
+           into = c("year","month","day"),
+           sep = "-")
 
-base_plot <- ggmap(sf_map)+
-  geom_point(data = sf_trees, mapping = aes(longitude, latitude),
-             color = "lightgreen",
+num_years <- length(unique(sf_plot_data$year))
+
+#base_map <- 
+ggmap(sf_map)+
+  geom_point(data = sf_plot_data, mapping = aes(longitude, latitude, color = year),
              size = 0.05,
              alpha = 0.2, 
-             na.rm = TRUE)
+             na.rm = TRUE)+
+  guides(color = FALSE)
   
   
-base_plot+
-  transition_states(date,
-                    transition_length = 3,
-                    state_length = 9)+
+animated_map <- base_map+
+  transition_time(year)+
   ease_aes('linear')+
   labs(title = "Trees of San Francisco",
-       subtitle = "Year: {round(frame_along,0)}",
+       subtitle = "Year: {closest_state}",
        x = "",
        y = "",
        caption = "Source: San Francisco Open Data Portal | Graph: @mathl3t3")
   
+gif_map <- animate(animated_map,
+                   renderer = altrenderer,
+                   nframes = num_years*10,
+                   fps = 10,
+                   height = 800,
+                   width = 800,
+                   rewind = FALSE)
+
+anim_save(animation = gif_map,
+          filename = here::here("trees.gif"))
+
+#NOT SAVING AS ANIMATION!!! SAVES PNG FILES
