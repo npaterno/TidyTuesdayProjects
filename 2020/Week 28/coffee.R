d1363aa0084a506fba8ab63cd2733e3891a42e1f
npaterno <nicholas.paterno@gmail.com>
2020-07-07 14:49:59 -0700

    different approach

--- 2020/Week 28/coffee.R
+++ 2020/Week 28/coffee.R
@@ -1,21 +1,38 @@
+# Load Packages
 library(tidyverse)
+library(grid)
+library(ggpubr)
 
+# Load data
 tuesdata <- tidytuesdayR::tt_load(2020, week = 28)
-
 coffee_ratings <- tuesdata$coffee_ratings
 
-country_summaries <- coffee_ratings %>% 
-  mutate(
-    country = case_when(
-      str_detect(country_of_origin, "United States") ~ "United States",
-      TRUE ~ country_of_origin 
-    )
-  )%>% 
-  group_by(country, species) %>% 
-  summarize(count = n(),
-            mean_overall = mean(total_cup_points)
-    
+# Remove NA data
+coffee_ratings <- coffee_ratings %>% 
+  na.omit() %>% 
+  pivot_longer(
+    cols = c("flavor","aftertaste","acidity","body","balance","uniformity","clean_cup","sweetness","cupper_points", "moisture"),
+    names_to = "category",
+    values_to = "score"
   )
 
-ggplot(country_summaries, aes(count, mean_overall))+
-  geom_point()
+
+# Plot mean altitude against acidity
+ggplot(
+  coffee_ratings,
+  aes(x = aroma, y = score)
+)+
+  geom_point()+
+  geom_smooth(
+    method = lm,
+   se = FALSE
+  )+
+  stat_cor(method = "pearson", label.x = 6, label.y = 10)+
+  labs(
+    title = "Can coffee aroma predict the other categories?",
+    x = "Aroma",
+    y = "Score",
+    caption = "Source: Coffee Qualit Database | Graph:@mathl3t3"
+  )+
+  facet_wrap(~category)
+
